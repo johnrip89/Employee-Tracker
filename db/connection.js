42986bdc0b005ba117ce6d85ad81c1ab4ac0f095
johnrip89 <lebronjohn89@gmail.com>
2021-10-02 12:14:43 -0600

    more fixes

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',        
         user: 'root',       
-        password: 'Larrydude89!',
+        password: '',
         database: 'employee_tracker'
     },
     console.log('Connected to the employee_tracker database.')
